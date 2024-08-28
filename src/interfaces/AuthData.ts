e43c27f8d655d3b5a1d10003ae488590d4f26501
Eric <ericgeorge4477@gmail.com>
2024-08-28 05:24:11 +0300

    Remove api folder and create interfaces folder

---
+++ src/interfaces/AuthData.ts
@@ -0,0 +1,12 @@
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface RegisterData {
+  first_name: string;
+  last_name: string;
+  password: string;
+  email: string;
+  role: string;
+}
