aeb1d54d0b3a661b231992ed50d840371160d967
Eric <ericgeorge4477@gmail.com>
2024-10-14 22:37:39 +0300

    Add animation and style changes to "Add to Cart" button on click

--- src/shared/BookCard.tsx
+++ src/shared/BookCard.tsx
@@ -2,21 +2,39 @@ import { Book } from "../interfaces/MasterData";
 import { useAppDispatch } from "../redux/hooks";
 import { getRandomBookImg } from "../constants/FUNCTIONS";
 import { getRandomBookAuthor } from "../constants/FUNCTIONS";
-import { memo } from "react";
+import { memo, useRef } from "react";
 import { addToCart } from "../redux/cartSlice";
 
 function BookCard({ book }: { book: Book }) {
   const dispatch = useAppDispatch();
   const bookImgSrc = getRandomBookImg();
   const bookAuthor = getRandomBookAuthor();
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
   const handleAddToCart = () => {
     dispatch(addToCart({ book: book._id, quantity: 1 }));
+
+    if (buttonRef.current) {
+      buttonRef.current.textContent = "Added to Cart";
+      buttonRef.current.style.backgroundColor = "green";
+      buttonRef.current.disabled = true;
+
+      // Reset the text after 1.5 seconds
+      setTimeout(() => {
+        buttonRef.current!.textContent = "Add to Cart";
+        buttonRef.current!.style.backgroundColor = "";
+        buttonRef.current!.disabled = false;
+      }, 1500);
+    }
   };
+
   return (
     <div className="book-card text-center max-w-[320px]">
       <div className="book-img-wrapper py-4 px-3 bg-white shadow-custom-light flex items-center justify-center relative">
         <img className=" w-full h-full" src={bookImgSrc} alt="book image" />
         <button
+          ref={buttonRef}
           onClick={handleAddToCart}
           className="add-to-cart-btn tracking-widest shadow-lg text-base max-[400px]:text-sm"
         >
